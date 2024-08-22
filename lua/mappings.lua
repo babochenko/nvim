6a7c001103052e8944e6476b579044b648f564e6
denisbabochenko <babochenko.denis2@gmail.com>
2024-08-23 00:13:09 +0100

    <leader>fr: run this file

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -21,6 +21,32 @@ map('n', '<leader>gB', ":Gitsigns blame<CR>", { desc = "[B]lame the whole buffer
 map('n', '<leader>gs', ":Git<CR>", { desc = "git [s]tatus" });
 map('n', '<leader>db', ":DBUI<CR>", { desc = "open data[b]ase ui" });
 
+map('n', '<leader>fr', function()
+  -- Get the current file path
+  local file_path = vim.fn.expand('%:p')
+
+  -- Get the file extension to determine how to run the file
+  local file_extension = vim.fn.expand('%:e')
+
+  -- Determine the command based on the file extension
+  local run_command
+
+  if file_extension == "lua" then
+      run_command = "lua " .. file_path
+  elseif file_extension == "py" then
+      run_command = "python3 " .. file_path
+  elseif file_extension == "sh" then
+      run_command = "bash " .. file_path
+  else
+      -- Add more conditions for other file types as needed
+      print("No command defined for this file type")
+      return
+  end
+
+  -- Open a terminal and run the command
+  vim.cmd("split | terminal " .. run_command .. " ; read")
+end, { desc = "[r]un this file" });
+
 map('n', '<leader>fg', function()
   require("telescope").extensions.live_grep_args.live_grep_args();
 end, { desc = "live [g]rep with args" });
