da2aad5266964158b43dbe8618a24f4253c5a53e
denisbabochenko <babochenko.denis2@gmail.com>
2025-02-01 14:18:16 +0000

    mymath: commands - e.g. Mul 12, Add 33, etc.

--- lua/mymath.lua
+++ lua/mymath.lua
@@ -17,6 +17,29 @@ local function _num_write(func)
   vim.fn.setline(start_pos[2], vim.fn.getline(start_pos[2]):sub(1, start_pos[3] - 1) .. result .. vim.fn.getline(end_pos[2]):sub(end_pos[3] + 1))
 end
 
+local function make_cmd(name, func)
+  vim.api.nvim_create_user_command(name, function(opts)
+    local factor = tonumber(opts.args)
+    if factor then
+      func(factor)
+    else
+      print("Invalid number")
+    end
+  end, { nargs = 1, range = true })
+end
+
+local MyMath = {
+  Add = function(factor) _num_write(function(num) return num + factor end) end,
+  Sub = function(factor) _num_write(function(num) return num - factor end) end,
+  Mul = function(factor) _num_write(function(num) return num * factor end) end,
+  Div = function(factor) _num_write(function(num) return num / factor end) end,
+  Pow = function(factor) _num_write(function(num) return num ^ factor end) end,
+}
+
+for name, func in pairs(MyMath) do
+  make_cmd(name, func)
+end
+
 function _G.add(factor)
   return _num_write(function(num) return num + factor end)
 end
